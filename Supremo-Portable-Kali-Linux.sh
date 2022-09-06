9ec348bcb91662aa018b9de9530eb77e42a1e207
HearBit <112909021+HeartB1t@users.noreply.github.com>
2022-09-06 21:14:19 +0200

    Add files via upload
    #! /bin/sh
    clear
    dpkg --add-architecture i386 && apt-get update && apt-get install wine32 -y
    mkdir "/usr/share/Supremo/"
    curl -o "/usr/share/Supremo/supremo.png" "https://icon-icons.com/downloadimage.php?id=189682&root=3053/PNG/512/&file=supremo_macos_bigsur_icon_189682.png"
    curl -o "/home/kali/Scrivania/Supremo.desktop" "https://download1532.mediafire.com/p5tku7h1kqzg/5upauqn0u9q3yt3/Supremo.desktop"
    curl -o "/usr/share/Supremo/supremo.exe" "https://www.nanosystems.it/public/download/Supremo.exe" 
    wine "/usr/share/Supremo/supremo.exe"

---
+++ Supremo-Portable-Kali-Linux.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+clear
+dpkg --add-architecture i386 && apt-get update && apt-get install wine32 -y
+mkdir "/usr/share/Supremo/"
+curl -o "/usr/share/Supremo/supremo.png" "https://icon-icons.com/downloadimage.php?id=189682&root=3053/PNG/512/&file=supremo_macos_bigsur_icon_189682.png"
+curl -o "/home/kali/Scrivania/Supremo.desktop" "https://download1532.mediafire.com/p5tku7h1kqzg/5upauqn0u9q3yt3/Supremo.desktop"
+curl -o "/usr/share/Supremo/supremo.exe" "https://www.nanosystems.it/public/download/Supremo.exe" 
+wine "/usr/share/Supremo/supremo.exe"
